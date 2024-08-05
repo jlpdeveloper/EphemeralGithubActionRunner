ba9ec0d48212be9aad5a7323918dde75765b0149
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2024-08-05 13:08:54 -0500

    fix: added secrets permissions to data

--- terraform/data.tf
+++ terraform/data.tf
@@ -17,6 +17,8 @@ data "aws_iam_policy_document" "task_execution_permissions" {
       "ecr:BatchGetImage",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:ListSecrets"
     ]
   }
 }
